c921e4010d9c873e8b6f2e8f41e0192580e07a94
ChangRiver <819669045@qq.com>
2016-12-29 09:59:10 +0800

    app create

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import App from './components/App';
+
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+
+const reducer = (state = defaultState, action) => {
+  return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
