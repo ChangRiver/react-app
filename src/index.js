329c7ff5ca053b19eb30ad3c9364a05124d472f5
ChangRiver <819669045@qq.com>
2016-12-29 10:47:16 +0800

    add custom http superagent

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
 import App from './components/App';
 
 const defaultState = {
@@ -10,10 +11,18 @@ const defaultState = {
 };
 
 const reducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'HOME_PAGE_LOADED':
+      return {
+        ...state,
+        articles: action.payload.articles
+      }
+  }
+
   return state;
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
