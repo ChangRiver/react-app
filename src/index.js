48fb639a7ff177eec4b623d65810fa8bc5d80629
ChangRiver <819669045@qq.com>
2016-12-29 11:22:36 +0800

    refactor store

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import { promiseMiddleware } from './middleware';
 import App from './components/App';
-
-const defaultState = {
-  appName: 'conduit',
-  articles: null
-};
-
-const reducer = (state = defaultState, action) => {
-  switch(action.type) {
-    case 'HOME_PAGE_LOADED':
-      return {
-        ...state,
-        articles: action.payload.articles
-      }
-  }
-
-  return state;
-};
-
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
